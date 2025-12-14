f3bd6728ffc2474c842bb2cce6355dad81f3d173
Ariel Wadyese <arielwadyese@gmail.com>
2025-12-14 12:42:41 -0600

    This is a fun project, basically has the logic to help bring 3d models into next JS

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -7,50 +7,27 @@ import { Bounds, ScrollControls } from '@react-three/drei'
 
 
 
-export default function Scene(){
-
-
-
-    return(
-
-    <Canvas 
-    
-    camera={{
-
+export default function Scene() {
+  return (
+    <Canvas
+      // Lower GPU pressure in browsers (helps avoid WebGL context lost on deploys)
+      dpr={[1, 1.5]}
+      gl={{ powerPreference: "high-performance", antialias: false }}
+      camera={{
         position: [0, 0, 200],
-        fov:11,
-        far:10000
-    }}
-
-    style={{width: "100vw", height: "100vh"}}
-    
+        fov: 11,
+        far: 10000,
+      }}
+      style={{ width: "100vw", height: "100vh" }}
     >
-        <directionalLight position={[-2, -5, 5]} intensity={4} />
-
-        
-        <Suspense fallback={null}>
-
-          <Bounds fit margin={1.4}>
-
-            <ScrollControls damping={0.2} pages={7}>
-
-                <Model scale={110} />
-
-            </ScrollControls>
-
-          </Bounds>
-
-
-         
-        </Suspense>
-
-
-
+      <directionalLight position={[-2, -5, 5]} intensity={4} />
+      <Suspense fallback={null}>
+        <Bounds fit margin={1.4}>
+          <ScrollControls damping={0.2} pages={7}>
+            <Model scale={110} />
+          </ScrollControls>
+        </Bounds>
+      </Suspense>
     </Canvas>
-
-        
-    )
-
-
-
+  );
 }
